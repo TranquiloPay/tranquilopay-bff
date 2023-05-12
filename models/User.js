1b0d4aa5749c9616a6525a93253a6d4c16cf8804
joaoGSO <you@example.com>
2023-05-12 15:45:37 -0300

    novos campos para registro de usuario

--- models/User.js
+++ models/User.js
@@ -2,7 +2,15 @@ const mongoose = require('mongoose')
 
 const User = mongoose.model('User', {
     name: String,
+    cpf: String,
+    state: String,
+    city: String,
+    street: String,
+    district: String,
+    number: String,
+    complement: String,
     email: String,
+    phone: String,
     password: String
 })
 
